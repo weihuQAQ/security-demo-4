b4ff8ad2fc0c5bde3e9f04053f77906dfe805272
huwei <18586626673@163.com>
2023-03-24 01:05:52 +0800

    feat: add mock http file & env file

--- src/main/java/com/hw/securitydemo4/entry/User.java
+++ src/main/java/com/hw/securitydemo4/entry/User.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class User implements UserDetails {
     @Id
     @GeneratedValue
-    private Integer id;
+    private String id;
     private String username;
     private String password;
     private String email;
